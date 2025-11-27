b71205a39c10b59e2230b3248bc6d9d7289a3ed6
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-27 09:25:50 +0100

    zadanie 5

---
+++ przestepny/Program.cs
@@ -0,0 +1,38 @@
+﻿int rok = 2024;
+
+if (rok % 4 == 0)
+
+{
+
+    Console.WriteLine("Rok przestępny");
+
+}
+
+else
+
+{
+
+    Console.WriteLine("Rok nieprzestępny");
+
+}
+
+
+
+
+rok = 2024;
+
+if (rok % 4 == 0 &&(rok%100 !=0|| rok % 400==0)) // lata przestepne nie wystepuja w latach setnych lecz wyjatkiem sa czterysetne
+
+{
+
+    Console.WriteLine("Rok przestępny");
+
+}
+
+else
+
+{
+
+    Console.WriteLine("Rok nieprzestępny");
+
+}
