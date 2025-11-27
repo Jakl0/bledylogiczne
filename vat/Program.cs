e259813ba479efed3dbe6d1c717d591071bdbea2
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-27 09:14:29 +0100

    zadanie 4

---
+++ vat/Program.cs
@@ -0,0 +1,19 @@
+﻿double cenaNetto = 100;
+
+double vat = 23; // 23% 
+
+double cenaBrutto = cenaNetto + vat;
+
+Console.WriteLine($"Cena brutto: {cenaBrutto}");
+
+
+
+
+
+cenaNetto = 100;
+
+vat = 23; // 23% 
+
+cenaBrutto = cenaNetto + cenaNetto*(vat/100); //To jest poprawny zapis procentu który działa na każdą liczbę
+
+Console.WriteLine($"Cena brutto: {cenaBrutto}");
