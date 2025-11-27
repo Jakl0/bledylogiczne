c9795461c8537593a0ccd637d314506f5bc4bacb
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-27 09:40:31 +0100

    zadanie 8

---
+++ haslo/Program.cs
@@ -0,0 +1,64 @@
+﻿string haslo = "Haslo123!";
+
+bool maMalaLitere = false;
+
+bool maDuzaLitere = false;
+
+bool maCyfre = false;
+
+bool maZnakSpecjalny = false;
+
+foreach (char znak in haslo)
+
+{
+
+    if (char.IsLower(znak)) maMalaLitere = true;
+
+    if (char.IsUpper(znak)) maDuzaLitere = true;
+
+    if (char.IsDigit(znak)) maCyfre = true;
+
+    if (!char.IsLetterOrDigit(znak)) maZnakSpecjalny = true;
+
+}
+
+
+
+bool silneHaslo = maMalaLitere || maDuzaLitere || maCyfre || maZnakSpecjalny;
+
+Console.WriteLine(silneHaslo ? "Silne" : "Słabe");
+
+
+
+
+
+
+string haslo1 = "Haslo123!";
+
+maMalaLitere = false;
+
+ maDuzaLitere = false;
+
+ maCyfre = false;
+
+maZnakSpecjalny = false;
+
+foreach (char znak in haslo)
+
+{
+
+    if (char.IsLower(znak)) maMalaLitere = true;
+
+    if (char.IsUpper(znak)) maDuzaLitere = true;
+
+    if (char.IsDigit(znak)) maCyfre = true;
+
+    if (!char.IsLetterOrDigit(znak)) maZnakSpecjalny = true;
+
+}
+
+
+
+silneHaslo = (maMalaLitere && maDuzaLitere && maCyfre && maZnakSpecjalny); //Musimy sprawdzić czy hasło zawiera wszystkie z rzeczy , a nie tylko jedną 
+
+Console.WriteLine(silneHaslo ? "Silne" : "Słabe");
---
+++ rzymskanaarabska/Program.cs
@@ -0,0 +1,37 @@
+﻿string rzymska = "XIV";
+
+Dictionary<char, int> wartosci = new Dictionary<char, int>
+
+{
+
+  {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}
+
+};
+
+int wynik = 0;
+
+for (int i = 0; i < rzymska.Length; i++)
+
+{
+
+    if (i < rzymska.Length - 1 &&
+
+      wartosci[rzymska[i]] < wartosci[rzymska[i + 1]])
+
+    {
+
+        wynik -= wartosci[rzymska[i]];
+
+    }
+
+    else
+
+    {
+
+        wynik += wartosci[rzymska[i]];
+
+    }
+
+}
+
+Console.WriteLine($"Wynik: {wynik}");
