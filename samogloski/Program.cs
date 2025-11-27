003512125e783d7570d38c7ba9ae9352d87e03b0
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-27 09:06:58 +0100

    zadanie 2

---
+++ samogloski/Program.cs
@@ -0,0 +1,43 @@
+﻿string slowo = "programowanie";
+
+int licznik = 0;
+
+foreach (char litera in slowo)
+
+{
+
+    if (litera == 'a' || litera == 'e' || litera == 'i' ||
+
+      litera == 'o' || litera == 'u')
+
+    {
+
+        licznik++;
+
+    }
+
+}
+Console.WriteLine($"Liczba samogłosek: {licznik}");
+
+slowo = "programowanie";
+
+licznik = 0;
+
+foreach (char litera in slowo)
+
+{
+
+    if (litera == 'a' || litera == 'e' || litera == 'i' ||
+
+      litera == 'o' || litera == 'u' || litera == 'ó' || litera == 'ą' || litera == 'ę'||litera =='y' || litera == 'A' || litera == 'E' || litera == 'I' ||
+
+      litera == 'O' || litera == 'U' || litera == 'Ó' || litera == 'Ą' || litera == 'Ę'||litera == 'Y') //uwzględniamy duże litery , polskie znaki i literę Y
+
+    {
+
+        licznik++;
+
+    }
+
+}
+Console.WriteLine($"Liczba samogłosek: {licznik}");
