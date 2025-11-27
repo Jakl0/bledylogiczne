2709f0426bae63ba2199bbe89aae96c89fa41240
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-27 09:10:32 +0100

    zadanie 3

---
+++ dodatnia/Program.cs
@@ -0,0 +1,37 @@
+﻿int liczba = 0;
+
+if (liczba > 0)
+
+{
+
+    Console.WriteLine("Liczba dodatnia");
+
+}
+
+else
+
+{
+
+    Console.WriteLine("Liczba ujemna");
+
+}
+
+
+liczba = 0;
+
+if (liczba > 0)
+
+{
+
+    Console.WriteLine("Liczba dodatnia");
+
+}
+else if (liczba == 0) Console.WriteLine("liczba nie jest ani dodatnia ani ujemna"); //dodajemy przypadek zera któro nie jest ani dodatnie ani ujemne
+
+else
+
+{
+
+    Console.WriteLine("Liczba ujemna");
+
+}
