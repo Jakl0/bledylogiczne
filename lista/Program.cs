8d19b51ed195e0627c494232906237d3ad2e8311
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-27 09:32:40 +0100

    zadanie 6

---
+++ lista/Program.cs
@@ -0,0 +1,26 @@
+﻿List<int> lista1 = new List<int> { 1, 2, 3, 4, 5 };
+
+List<int> lista2 = new List<int> { 1, 2, 3, 4, 5 };
+
+bool saRowne = lista1 == lista2;
+
+Console.WriteLine(saRowne ? "Równe" : "Różne");
+
+
+
+
+List<int> lista3 = new List<int> { 1, 2, 3, 4, 5 };
+
+List<int> lista4 = new List<int> { 1, 2, 3, 4, 5 };
+
+saRowne = true;
+for(int i=0; i > 5; i++) //sprawdzanie w poprzednim przypadku nie działa , trzeba sprawdzić każdą liczbę osobno
+{
+    if (lista3[i] != lista4[i])
+    {
+        saRowne = false;
+        break;
+    }
+}
+
+Console.WriteLine(saRowne ? "Równe" : "Różne");
