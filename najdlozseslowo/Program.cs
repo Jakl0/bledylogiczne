d4543e973e3c86b8a21007c69798a0bf86973c2b
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-27 09:47:34 +0100

    zadanie 9

---
+++ najdlozseslowo/Program.cs
@@ -0,0 +1,23 @@
+﻿string zdanie = "Programowanie w C# jest bardzo ciekawe";
+
+string[] slowa = zdanie.Split(' ');
+
+string najdluzsze = "";
+
+foreach (string slowo in slowa)
+
+{
+
+    if (slowo.Length >= najdluzsze.Length)
+
+    {
+
+        najdluzsze = slowo;
+
+    }
+
+}
+
+Console.WriteLine($"Najdłuższe słowo: {najdluzsze}");
+
+
