b5fe7450e222dfc500a3356060c2a11f19d14215
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-27 08:59:58 +0100

    Dodaj pliki projektów.

---
+++ prostokat/Program.cs
@@ -0,0 +1,18 @@
+﻿int dlugosc = 10;
+
+int szerokosc = 5;
+
+int obwod = dlugosc + szerokosc;
+
+Console.WriteLine($"Obwód: {obwod}");
+
+
+
+
+int dlugosc1 = 10;
+
+int szerokosc1 = 5;
+
+int obwod1 = 2*dlugosc1 + 2*szerokosc1; //wzor na obwod prostokata to obw = 2a + 2b
+
+Console.WriteLine($"Obwód: {obwod1}");
